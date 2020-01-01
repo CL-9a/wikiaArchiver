aaf06a5972110e3e2208ab2cdeb991e4474ea46f
cl-9a <56308938+CL-9a@users.noreply.github.com>
2020-01-01 12:39:13 +0100

    handle chunked-down allpages

--- functions.sh
+++ functions.sh
@@ -4,11 +4,43 @@ decodeURL() {
 
 getAllPages() {
 	local wikiUrl="$1"
+	local relPageUrl="/wiki/Special:AllPages"
 
-	local html="$(curl --silent --location "$wikiUrl/wiki/Special:AllPages")"
-	local links="$(echo "$html" | pup 'table.mw-allpages-table-chunk a attr{href}')"
+	_getAllPagesChunks "$wikiUrl" "$relPageUrl"
+}
 
-	echo "$links"
+_getAllPagesChunks () {
+	local wikiUrl="$1"
+	local relPageUrl="$2"
+	local toQuery="${1}${2}"
+
+	#>&2 echo "_getAllPagesChunks $toQuery"
+
+	local html="$(curl --silent --location -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0' "$toQuery")"
+
+	local htmlAllPagesList="$(echo "$html" | pup 'table.allpageslist a attr{href}' | uniq)"
+	if [[ -z "$htmlAllPagesList" ]]; then
+		#>&2 echo "no allpageslist - got a page list chunk:"
+
+		local chunk="$(echo "$html" | pup 'table.mw-allpages-table-chunk a attr{href}')"
+		#>&2 echo "$chunk"
+		echo "$chunk"
+	else
+		#>&2 echo "has allpageslist - got a list of page lists: $htmlAllPagesList"
+
+		local allPagesChunks=""
+		for url in $htmlAllPagesList; do
+			sleep 1
+
+			#>&2 echo "recursively getting allPages from $wikiUrl$url"
+			local lowerPageChunks="$(_getAllPagesChunks "$wikiUrl$url")"
+
+			#>&2 echo "adding to main list"
+			allPagesChunks+="\n$lowerPageChunks"
+		done
+		#>&2 echo "all done, sending to parent"
+		echo "$allPagesChunks"
+	fi
 }
 
 getPageHistoryLink () {
